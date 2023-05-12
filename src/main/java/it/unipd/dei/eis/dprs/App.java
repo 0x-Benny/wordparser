49b6fc6e7b6e640e9271e0de8baf4bce90c41376
benny <bernardo.simonetto@gmail.com>
2023-05-12 10:26:36 +0200

    First commit

---
+++ src/main/java/it/unipd/dei/eis/dprs/App.java
@@ -0,0 +1,13 @@
+package it.unipd.dei.eis.dprs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
