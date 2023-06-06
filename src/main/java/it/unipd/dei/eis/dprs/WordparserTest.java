e4dabd94c14ca90476c5e5d5657640eb97d4daa8
Benny <bernardo.simonetto@gmail.com>
2023-06-06 14:38:21 +0200

    add assets

--- src/main/java/it/unipd/dei/eis/dprs/WordparserTest.java
+++ src/main/java/it/unipd/dei/eis/dprs/WordparserTest.java
@@ -11,10 +11,10 @@ public class WordparserTest
         TheGuardianAdapter api = new TheGuardianAdapter("9b04d5bd-ae24-4ed7-8bde-7c57ee902f70");
         ArrayList<BasicArticle> database = api.fetchArticles();
 
-        NYTimesAdapter csv = new NYTimesAdapter("./src/assets/nytimes/");
+        NYTimesAdapter csv = new NYTimesAdapter("./assets/nytimes/");
         database.addAll(csv.fetchArticles());
 
-        TreeMap<String, Integer> frequency = Wordparser.wordCount(Wordparser.filterWords(database, "./src/assets/utilities/english_stoplist_v1.txt"));
+        TreeMap<String, Integer> frequency = Wordparser.wordCount(Wordparser.filterWords(database, "./assets/utilities/english_stoplist_v1.txt"));
         System.out.println(frequency);
     }
 }
