5b3f08c7aceee6d0b8ec2602772c37cde4f9d66d
F-E-R <francescoelia.rizzo@studenti.unipd.it>
2023-06-13 10:51:02 +0200

    IT: Rivisti alcuni commenti.
    EN: Some comments have been rewritten.

--- src/main/java/it/unipd/dei/eis/dprs/tools/Deserializer.java
+++ src/main/java/it/unipd/dei/eis/dprs/tools/Deserializer.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 public class Deserializer
 {
   /**
-   * Deserializza il file JSON, ovvero mappa i campi di ogni articolo nei corrispondenti campi di un oggetto.
+   * Deserializza il file JSON, ovvero mappa gli attributi JSON di ogni articolo nei corrispondenti campi di un oggetto.
    * @param databasePath Percorso del database (file JSON).
    * @return Array di articoli.
    * @see com.fasterxml.jackson
